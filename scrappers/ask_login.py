0fd3a26ec92bee4052da2eda7cae0dd172b1f3a0
omarallam22 <omarallam@std.mans.edu.eg>
2024-08-17 14:26:15 +0000

    adding requirements.txt and updating scrappers/ask_login.py

--- scrappers/ask_login.py
+++ scrappers/ask_login.py
@@ -4,40 +4,40 @@ from bs4 import BeautifulSoup
 import time
 
 class AskLogIn:
-    def __init__(self, chrome_driver_path, profile_link=None):
+  def __init__(self, chrome_driver_path, profile_link=None):
+    self.profile_link = profile_link
+    WINDOW_SIZE = "500,800"
+    driver_location = "/usr/bin/chromedriver"
+    binary_location = "/usr/bin/google-chrome"
+    self.chrome_options = Options()
+    self.chrome_options.add_argument('--no-sandbox')
+    #self.chrome_options.add_argument("--headless")
+    self.chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
+    self.chrome_options.binary_location = binary_location
+    self.driver = webdriver.Chrome(executable_path=driver_location,
+                                    chrome_options=self.chrome_options) 
+    self.driver.get("https://linkedin.com/uas/login")
+    while(True):
+        if (self.driver.current_url == "https://www.linkedin.com/feed/"):
+            break
+    print("✔LogIn Succeeded✔")
 
-        self.profile_link = profile_link
-        
-        WINDOW_SIZE = "500,800"
-        self.chrome_options = Options()
-        #self.chrome_options.add_argument("--headless")
-        self.chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
-        self.driver = webdriver.Chrome(executable_path=chrome_driver_path,
-                                       chrome_options=self.chrome_options) 
-        self.driver.get("https://linkedin.com/uas/login")
-        while(True):
-            if (self.driver.current_url == "https://www.linkedin.com/feed/"):
+    if self.profile_link:
+        self.driver.get(self.profile_link)
+    else:
+        src = self.driver.page_source
+        soup = BeautifulSoup(src, "lxml")
+        while True:
+            if self.profile_link:
                 break
-        print("✔LogIn Succeeded✔")
-        
+            try:
+                self.profile_link = "https://www.linkedin.com" + soup.find("div",{"class":"scaffold-layout__sidebar"}).find("a").get('href')
+                self.driver.get(self.profile_link)
+            except:
+                pass
 
-        if self.profile_link:
-            self.driver.get(self.profile_link)
-        else:
-            src = self.driver.page_source
-            soup = BeautifulSoup(src, "lxml")
-            while True:
-                if self.profile_link:
-                    break
-                try:
-                    self.profile_link = "https://www.linkedin.com" + soup.find("div",{"class":"scaffold-layout__sidebar"}).find("a").get('href')
-                    self.driver.get(self.profile_link)
-                except:
-                    pass
+  def get_driver(self):
+      return self.driver
 
-    def get_driver(self):
-        return self.driver
-    
-    def get_profile_link(self):
-        return self.profile_link
-
+  def get_profile_link(self):
+      return self.profile_link

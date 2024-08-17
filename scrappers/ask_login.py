d30ab0ca2027a462634a2feb9036b55042321617
Omar Khalid Allam <91850794+OmarAllam22@users.noreply.github.com>
2024-08-17 18:09:33 +0000

    add option to either run on linux or windows

--- scrappers/ask_login.py
+++ scrappers/ask_login.py
@@ -4,18 +4,30 @@ from bs4 import BeautifulSoup
 import time
 
 class AskLogIn:
-  def __init__(self, chrome_driver_path, profile_link=None):
+  def __init__(self, chrome_driver_path, profile_link=None, os="windows"):
     self.profile_link = profile_link
     WINDOW_SIZE = "500,800"
-    driver_location = "/usr/bin/chromedriver"
-    binary_location = "/usr/bin/google-chrome"
+
     self.chrome_options = Options()
     self.chrome_options.add_argument('--no-sandbox')
     #self.chrome_options.add_argument("--headless")
     self.chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
-    self.chrome_options.binary_location = binary_location
-    self.driver = webdriver.Chrome(executable_path=driver_location,
+    if os == "windows":
+    """
+    executable file for chrome-driver on windows is already bundled with this repo.
+    """
+      self.driver = webdriver.Chrome(executable_path=chrome_driver_path,
                                     chrome_options=self.chrome_options) 
+    elif os == "linux":
+      """
+      This runs on linux after installing the chromedriver and google-chrome on the given paths. 
+      """
+      driver_location = "/usr/bin/chromedriver"
+      binary_location = "/usr/bin/google-chrome"
+      self.chrome_options.binary_location = binary_location
+      self.driver = webdriver.Chrome(executable_path=driver_location,
+                                    chrome_options=self.chrome_options)
+    
     self.driver.get("https://linkedin.com/uas/login")
     while(True):
         if (self.driver.current_url == "https://www.linkedin.com/feed/"):
